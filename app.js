e21da64efdda960ddb8585cefa5fd221dc7bc797
Priyanshu Choudhary <pc9781710@gmail.com.com>
2023-12-07 18:09:53 +0530

    On Passport Chapter

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 // Types of Encryptions 
 // Level 1 -> Simple storage of user data in database
 // Level 2 -> Using mongoose-encryption to encrypt password using encryption key
-// Level 3 -> Some data from code can be stored in external .env {environment variable} file such as encryption key ,api key etc
-// Level 1
+//         -> Some data from code can be stored in external .env {environment variable} file such as encryption key ,api key etc
+// Level 3 -> Hashing using Md5 
+// Level 4 -> Hashing using bcrypt / Salting / Salting Rounds 
+// Level 5 -> Passports
 
 //jshint esversion:6
 require('dotenv').config();
@@ -12,17 +14,33 @@ const express = require("express");
 const bodyParser =require("body-parser");
 const ejs =require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");//No need for "passport-local" it will be required by "passport-local-mongoose"
 
 const app = express();
 const port =3000;
 
-console.log(process.env.SECRET);
+// console.log(process.env.SECRET);
 
 //middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine','ejs');
+
+
+app.use(session({
+secret:"Priyanshu is the king",
+resave: false,
+saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
@@ -40,12 +58,19 @@ const secretSchema = new mongoose.Schema({
     }
 });
 
+secretSchema.plugin(passportLocalMongoose);
+
 const secret = process.env.SECRET;
 //here scret is is an encryption key using which passworrd is encrypted
 
-secretSchema.plugin(encrypt, { secret: secret,encryptedFields:["password"] });//*********** */
+//Level - 5 
+const User = mongoose.model("Users",secretSchema);
 
-const secretModel = mongoose.model("Users",secretSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/",(req,res)=>{
 res.render("home.ejs");
@@ -64,7 +89,7 @@ app.get("/logout",(req,res)=>{
 // const email = req.body.username;
 // const password = req.body.password;
 
-// secretModel.find({email:email})
+// User.find({email:email})
 // .then(foundUser=>{
 //      if(foundUser.password === password)
 //     {
@@ -78,27 +103,9 @@ app.get("/logout",(req,res)=>{
 // });
 
 app.post("/login", async (req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
-
-    try {
-        const foundUsers = await secretModel.find({ email: email });//will return an array
- console.log(foundUsers);
-        if (foundUsers.length > 0) {
-            const foundUser = foundUsers[0];
-
-            if (foundUser.password === password) {
-                res.render("secrets");
-            } else {
-                res.send("Incorrect password");
-            }
-        } else {
-            res.send("User not found");
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
-    }
+    //1. check user exits or not in database using email
+    //2 if yes then bcrypt.compare user entered password with hash saved in db that is foundUser.password
+    
 });
 
 // User.findById(id)
@@ -109,10 +116,12 @@ app.post("/login", async (req, res) => {
 //   done(err, null);
 // });
 app.post("/register",async (req,res)=>{
-const newUser = new secretModel(
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+const newUser = new User(
 {
      email : req.body.username,
-     password : req.body.password
+     password : hash
 });
 try{
 newUser.save();
@@ -123,6 +132,9 @@ catch(err)
     console.log(err);
 }
 });
+
+});
+// console.log(md5("priyanshu123"));
 
 app.listen(port,()=>{
     console.log(`Server running at port : ${port}`);
